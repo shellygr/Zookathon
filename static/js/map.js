c8faafd35b09a2d74c2059ff37bd9a0f776571d2
ShellyG <shellygr151@gmail.com>
2016-09-01 20:33:57 +0300

    search box fix

--- static/js/map.js
+++ static/js/map.js
@@ -49,10 +49,10 @@ function initMap() {
   map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
 
   // Bias the SearchBox results towards current map's viewport.
-  map.addListener('bounds_changed', function() {
+/*  map.addListener('bounds_changed', function() {
     searchBox.setBounds(map.getBounds());
   });
-
+*/
   // Listen for the event fired when the user selects a prediction and retrieve
   // more details for that place.
   searchBox.addListener('places_changed', function() {
