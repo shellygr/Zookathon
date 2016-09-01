753915cb3fc102ed7c38d2a2bc30dc936d0d26d2
Keren Nemzer <kristina.georg@gmail.com>
2016-09-01 20:08:52 +0300

    add search box

--- static/js/map.js
+++ static/js/map.js
@@ -43,6 +43,43 @@ function initMap() {
     zoom: 3
   });
 
+    // Create the search box and link it to the UI element.
+  var input = document.getElementById('search-input');
+  var searchBox = new google.maps.places.SearchBox(input);
+  map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
+
+  // Bias the SearchBox results towards current map's viewport.
+  map.addListener('bounds_changed', function() {
+    searchBox.setBounds(map.getBounds());
+  });
+
+  // Listen for the event fired when the user selects a prediction and retrieve
+  // more details for that place.
+  searchBox.addListener('places_changed', function() {
+    var places = searchBox.getPlaces();
+
+    if (places.length == 0) {
+      return;
+    }
+
+    // For each place, get the icon, name and location.
+    var bounds = new google.maps.LatLngBounds();
+    places.forEach(function(place) {
+      if (!place.geometry) {
+        console.log("Returned place contains no geometry");
+        return;
+      }
+
+      if (place.geometry.viewport) {
+        // Only geocodes have viewport.
+        bounds.union(place.geometry.viewport);
+      } else {
+        bounds.extend(place.geometry.location);
+      }
+    });
+    map.fitBounds(bounds);
+  });
+
   getFromDb(map);
 
   var contentString = '<div id="content">'+
